148ac044394212ecae3989229104fb3158c98602
Timofey Chebotin <proehidna220@gmail.com>
2021-04-17 18:37:53 +0500

    first commit

---
+++ ChatHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Chat_Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(Chat_WCF.ServiceChat)))
+            {
+                host.Open();
+                Console.WriteLine("Хост стартовал!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
